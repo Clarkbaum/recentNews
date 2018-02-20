c640cc83f19cb29a232f1d2aa103509d92bf432d
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2018-02-20 14:53:53 -0600

    3 api putting all res into one array in state

--- components/App.js
+++ components/App.js
@@ -1,23 +1,45 @@
 import React from 'react';
+import Story from './Story.js';
 
 class App extends React.Component{
+
   constructor (props) {
     super(props);
     this.state = {
-      data: []
+      newList: []
     }
   }
 
   componentDidMount() {
+    let completeList = [];
     $.get('https://newsapi.org/v2/top-headlines?sources=abc-news&apiKey=0743d3c85acf40bf9eaf125623ca3ed5')
-    .done(function(res) {
-      console.log("res", res)
+    .done((res) => {
+      completeList = completeList.concat(res.articles)
+      $.get('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=0743d3c85acf40bf9eaf125623ca3ed5')
+      .done((res) => {
+        completeList = completeList.concat(res.articles)
+        $.get('https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=0743d3c85acf40bf9eaf125623ca3ed5')
+        .done((res) => {
+          completeList = completeList.concat(res.articles)
+          console.log("completeList", completeList)
+          console.log("this", this)
+          this.setState({
+            newList: completeList
+          })
+        })
+        .catch(() => console.log('error with GET CNN news'))
+      })
+      .catch(() => console.log('error with GET BBC news'))
     })
+    .catch(() => console.log('error with ABC news'))
   }
 
   render (){
     return (
-      <div> this is my new react app </div>
+      <div>
+        <h1> Recent News </h1>
+        <Story />
+      </div>
     )
   }
 }
---
+++ components/Story.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Story extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state  = {
+      data: []
+    }
+  }
+  render() {
+    return(
+      <div>
+        Story test
+      </div>
+    )
+  }
+}
+
+export default Story;
