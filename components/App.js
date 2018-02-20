b500212eb8a43d4cdfefe81aae12240682e473df
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2018-02-20 13:58:42 -0600

    get to abc news

--- components/App.js
+++ components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-export default class App extends React.Component{
+class App extends React.Component{
+  constructor (props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    $.get('https://newsapi.org/v2/top-headlines?sources=abc-news&apiKey=0743d3c85acf40bf9eaf125623ca3ed5')
+    .done(function(res) {
+      console.log("res", res)
+    })
+  }
+
   render (){
-    return <p> this is my new react app </p>
+    return (
+      <div> this is my new react app </div>
+    )
   }
 }
+
+export default App
