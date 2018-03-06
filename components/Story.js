9cbdcd89af4a59def19b89d86a5b3b1fcb7f60a4
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2018-03-06 13:02:07 -0600

    converted to stateless

--- components/Story.js
+++ components/Story.js
@@ -1,39 +1,32 @@
 import React from 'react';
 import '../styles.less';
 
-class Story extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state  = {
-      data: []
-    }
-  }
-  render() {
-    console.log("this.props", this.props.story)
-    return(
-      <div>
-        <table>
+const Story = props => {
+  return (
+    <div>
+      <table>
+        <tbody>
           <tr>
             <td>
-              <img className='image' src={this.props.story.urlToImage}></img>
+              <img className='image' src={props.story.urlToImage}></img>
             </td>
             <td className='data'>
               <div className='top'>
-                <div className='title'>{this.props.story.title}</div>
-                <div className='author'>Source: {this.props.story.author}</div>
-                <div className='description'>{this.props.story.description}</div>
+                <div className='title'>{props.story.title}</div>
+                <div className='author'>Source: {props.story.author}</div>
+                <div className='description'>{props.story.description}</div>
               </div>
               <div className='bottom'>
-                <a className='url' href={this.props.story.url}>{this.props.story.url}</a>
-                <div className='published'>{this.props.story.publishedAt}</div>
+                <a className='url' href={props.story.url}>{props.story.url}</a>
+                <div className='published'>{props.story.publishedAt}</div>
               </div>  
             </td>
           </tr>
-        </table>
-        <p />
-      </div>
-    )
-  }
+        </tbody>
+      </table>
+      <p />
+    </div>
+  )
 }
 
 export default Story;
